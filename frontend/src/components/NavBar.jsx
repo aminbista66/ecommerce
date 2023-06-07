86ba61a7fda6c217fdac93bc0cb37b8e4ec97e14
Amin Bista <aminbista6666@gmail.com>
2023-06-07 10:30:15 +0545

    initial: react project bootstrap

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Container = styled.div``
+
+function NavBar() {
+  return (
+    <>
+    <Container>
+        NavBar
+    </Container>
+    </>
+  )
+}
+
+export default NavBar
