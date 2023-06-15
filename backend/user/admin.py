03621f0adc7ccc90ce03a292644313b7bf8e37e6
Amin Bista <aminbista6666@gmail.com>
2023-06-15 13:50:25 +0545

    user creating via email

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    fields = ('email', 'first_name', 'is_superuser', 'date_joined',)
+    list_display = fields
+    ordering = ('date_joined',)
+    search_fields = ('email', 'first_name',)
+
+admin.site.register(User, UserAdmin)
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .model_manager import UserManager
 
 class User(AbstractUser):
-    firstName = models.CharField(max_length=255, null=False, blank=False)
-    lastName = models.CharField(max_length=255)
+    username = None
     email = models.EmailField(unique=True, null=False, blank=False)
 
+    objects = UserManager()
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['firstName']
+    REQUIRED_FIELDS = ['first_name']
 
     def __str__(self) -> str:
         return self.email
